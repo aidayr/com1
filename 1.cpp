4924dee740abcf43d51cbf1fe3f0bd237fa52b60
aidayr <pikpolina06@gmail.com>
2025-05-29 18:33:32 +0400

    Added program for Fibonacci number

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int fbn(int n){
+    if (n == 1 || n == 2) return 1;
+    return (fbn(n - 2) + fbn(n - 1));
+}
 int main(){
-    cout << "Hello, world!";
+    cout << "Hello, world!" << endl;
+    int n; cin >> n;
+    cout << fbn(n);
     return 0;
 }
