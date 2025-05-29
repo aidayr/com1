7039dc3cd8ebee1c2bfe96139713da6d5e939430
aidayr <pikpolina06@gmail.com>
2025-05-29 18:38:24 +0400

    Changed first number of sequence

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int fbn(int n){
-    if (n == 1 || n == 2) return 1;
+    if (n == 1) return 0;
+    if (n == 2) return 1;
     return (fbn(n - 2) + fbn(n - 1));
 }
 int main(){
